8d5c1dc7c93c88e342826475e4fd7d0e30778e5e
ptt <ptt99931.work@gmail.com>
2025-03-25 15:18:08 +0700

    [ui] set landscape screen mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:klondike_flutter_game/klondike_game.dart';
 
 void main() {
   final game = KlondikeGame();
-  runApp(GameWidget(game: game));
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]).then((_) {
+    runApp(GameWidget(game: game));
+  });
 }
